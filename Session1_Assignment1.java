2d2a02a757a74a86ad475b4f19664a032d8502d9
arpchauh <arpitchauhan10@gmail.com>
2016-09-18 12:20:30 +0530

    Add files via upload

---
+++ Session1_Assignment1.java
@@ -0,0 +1,16 @@
+package aatika;
+
+public class Session1_Assignment1 {
+
+	public static void main(String[] args) {
+		int a =20;
+		float b = 30.8f;
+		float c = a+b;
+		int d;
+		
+		d = (int)c;
+		
+		System.out.println("The sum of a and b is : "+d);
+	}
+
+}
